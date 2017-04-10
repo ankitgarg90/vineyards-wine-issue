a2dd5d3e43464fc3fcd72fa34a7814e28ab7fcc0
unknown <ankitgarg90@gnail.com>
2017-04-10 08:09:29 +0530

    final commit

--- index.php
+++ index.php
@@ -4,17 +4,17 @@ set_time_limit(0); // large execution time
 /**
  * Problem --
  
-   A large group of friends from the town of Nocillis visit the vineyards of Apan to taste wines. The vineyards produce many fine wines and the friends decide to buy as many as 3 bottles of wine each if they are available to purchase. Unfortunately, the vineyards of Apan have a peculiar restriction that they can not sell more than one bottle of the same wine. So the vineyards come up with the following scheme: They ask each person to write down a list of up to 10 wines that they enjoyed and would be happy buying. 
+  * A large group of friends from the town of Nocillis visit the vineyards of Apan to taste wines. The vineyards produce many fine wines and the friends decide to buy as many as 3 bottles of wine each if they are available to purchase. Unfortunately, the vineyards of Apan have a peculiar restriction that they can not sell more than one bottle of the same wine. So the vineyards come up with the following scheme: They ask each person to write down a list of up to 10 wines that they enjoyed and would be happy buying. 
  
  * Input - 
  
-   A two-column TSV file with the first column containing the ID (just a string) of a person and the second column the ID of the wine that they like. Example - 
+   * A two-column TSV file with the first column containing the ID (just a string) of a person and the second column the ID of the wine that they like. Example - 
 
 	https://s3.amazonaws.com/br-user/puzzles/person_wine_3.txt 
 
  * Expected Output -
  
-   First line contains the number of wine bottles sold in aggregate with your solution. Each subsequent line should be two columns, tab separated. The first column is an ID of a person and the second column should be the ID of the wine that they will buy.
+   * First line contains the number of wine bottles sold in aggregate with your solution. Each subsequent line should be two columns, tab separated. The first column is an ID of a person and the second column should be the ID of the wine that they will buy.
    
    
  * @package    WineTaste (Wine Allotment Problem)
